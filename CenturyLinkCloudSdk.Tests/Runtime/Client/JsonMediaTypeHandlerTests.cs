f55578f8a87f4729989a03f176f8395a22a31a99
Dan Schwabe <dfschwabe@yahoo.com>
2015-06-28 14:53:41 -0500

    specify accepted media type in request

---
+++ CenturyLinkCloudSdk.Tests/Runtime/Client/JsonMediaTypeHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using CenturyLinkCloudSdk.Runtime.Client;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+
+namespace CenturyLinkCloudSdk.Tests.Runtime.Client
+{
+    [TestFixture]
+    public class JsonMediaTypeHandlerTests
+    {
+        [Test]
+        public void SendAsync_AddsAcceptHeader()
+        {
+            var request = new HttpRequestMessage();
+            var innerHandler = new Mock<HttpClientHandler>();
+
+            innerHandler.Protected()
+                        .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                        .Returns(Task.FromResult(new HttpResponseMessage()));
+
+            new HttpMessageInvoker(new JsonMediaTypeHandler() { InnerHandler = innerHandler.Object })
+                .SendAsync(request, CancellationToken.None)
+                .Wait();
+
+            Assert.Greater(request.Headers.Accept.Count, 0);
+            Assert.AreEqual("application/json", request.Headers.Accept.First().MediaType);
+        }
+    }
+}
---
+++ CenturyLinkCloudSdk/Runtime/Client/JsonMediaTypeHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CenturyLinkCloudSdk.Runtime.Client
+{
+    public class JsonMediaTypeHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers
+                   .Accept
+                   .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
