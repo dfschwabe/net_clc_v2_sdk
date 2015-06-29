16cdf2a27a4b68730e29cfeef8c130a76ca226b4
Dan Schwabe <dfschwabe@yahoo.com>
2015-06-28 22:55:55 -0500

    correct content-type for json message handler

--- CenturyLinkCloudSdk.Tests/Runtime/Client/JsonMediaTypeHandlerTests.cs
+++ CenturyLinkCloudSdk.Tests/Runtime/Client/JsonMediaTypeHandlerTests.cs
@@ -29,5 +29,23 @@ namespace CenturyLinkCloudSdk.Tests.Runtime.Client
             Assert.Greater(request.Headers.Accept.Count, 0);
             Assert.AreEqual("application/json", request.Headers.Accept.First().MediaType);
         }
+
+        [Test]
+        public void SendAsync_AddsContentTypeHeader_WhenContentNotNull()
+        {
+            var request = new HttpRequestMessage{Content = new StringContent(string.Empty)};
+            var innerHandler = new Mock<HttpClientHandler>();
+
+            innerHandler.Protected()
+                        .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                        .Returns(Task.FromResult(new HttpResponseMessage()));
+
+            new HttpMessageInvoker(new JsonMediaTypeHandler() { InnerHandler = innerHandler.Object })
+                .SendAsync(request, CancellationToken.None)
+                .Wait();
+
+            Assert.AreEqual(1, request.Content.Headers.Count());
+            Assert.AreEqual("application/json; charset=utf-8", request.Content.Headers.GetValues("Content-Type").FirstOrDefault());
+        }
     }
 }
--- CenturyLinkCloudSdk/Runtime/Client/JsonMediaTypeHandler.cs
+++ CenturyLinkCloudSdk/Runtime/Client/JsonMediaTypeHandler.cs
@@ -7,11 +7,16 @@ namespace CenturyLinkCloudSdk.Runtime.Client
 {
     public class JsonMediaTypeHandler : DelegatingHandler
     {
+        private const string Json = "application/json";
+
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            request.Headers
-                   .Accept
-                   .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(Json));
+
+            if (request.Content != null)
+            {
+                request.Content.Headers.ContentType.MediaType = Json;
+            }
 
             return await base.SendAsync(request, cancellationToken);
         }
