a21be19e09e1269fd9f6237464a70f6f8faf98a5
Dan Schwabe <dfschwabe@yahoo.com>
2015-07-13 22:58:34 -0500

    mocking alert policy api

---
+++ CenturyLinkCloudSdk.UAT/Mock/Controllers/AlertPolicyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CenturyLinkCloudSdk.UAT.Mock.Controllers
+{
+    [TokenAuthorizationFilter]
+    public class AlertPolicyController : ApiController
+    {
+        [Route("alertpolicies/{alias}")]
+        public HttpResponseMessage Post([FromUri] string alias, [FromBody] MockAlertPolicy policy)
+        {
+            policy.id = Guid.NewGuid().ToString();
+            Users.ByAccountAlias[alias].AlertPolicies.Add(policy);
+
+            return Request.CreateResponse(HttpStatusCode.OK, policy);
+        }
+    }
+
+    public class MockAlertPolicy
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public IEnumerable<MockAlertAction> actions { get; set; }
+
+        public IEnumerable<MockAlertTrigger> triggers { get; set; }
+    }
+
+    public class MockAlertAction
+    {
+        public string action { get; set; }
+
+        public MockAlertActionSettings settings { get; set; }
+    }
+
+    public class MockAlertActionSettings
+    {
+        public IEnumerable<string> recipients { get; set; }
+    }
+
+    public class MockAlertTrigger
+    {
+        public string metric { get; set; }
+
+        public TimeSpan duration { get; set; }
+
+        public int threshold { get; set; }
+    }
+}
