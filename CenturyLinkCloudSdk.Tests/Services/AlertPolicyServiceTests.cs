1588411db282c344d7bc33d8292c6f97cc1f553e
Dan Schwabe <dfschwabe@yahoo.com>
2015-07-13 23:39:55 -0500

    implement alert policy service

---
+++ CenturyLinkCloudSdk.Tests/Services/AlertPolicyServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CenturyLinkCloudSdk.Models;
+using CenturyLinkCloudSdk.Runtime;
+using CenturyLinkCloudSdk.Runtime.Client;
+using CenturyLinkCloudSdk.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace CenturyLinkCloudSdk.Tests.Services
+{
+    [TestFixture]
+    public class AlertPolicyServiceTests
+    {
+        private const string AccountAlias = "alias";
+        private AlertPolicyService _testObject;
+        private Mock<IHttpClient> _client;
+        private Mock<IAliasProvider> _aliasProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new Mock<IHttpClient>();
+            _aliasProvider = new Mock<IAliasProvider>();
+            _aliasProvider.Setup(x => x.GetAccountAlias())
+                          .Returns(Task.FromResult(AccountAlias));
+            _testObject = new AlertPolicyService(_client.Object, _aliasProvider.Object);
+        }
+
+        [Test]
+        public void Create_PerformsCorrectRequest()
+        {
+            var requestUri = String.Format("alertpolicies/{0}", AccountAlias);
+            var expectedBody = new AlertPolicyDefniition();
+            var expectedToken = new CancellationTokenSource().Token;
+
+            _client.Setup(x => x.PostAsync<AlertPolicy>(requestUri, expectedBody, expectedToken))
+                   .Returns(Task.FromResult(new AlertPolicy()));
+
+            _testObject.Create(expectedBody, expectedToken).Wait();
+
+            _client.VerifyAll();
+        }
+
+        [Test]
+        public void Create_ReturnsExpectedResult()
+        {
+            var expectedResult = new AlertPolicy();
+
+            _client.Setup(x => x.PostAsync<AlertPolicy>(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<CancellationToken>()))
+                   .Returns(Task.FromResult(expectedResult));
+
+            var actualResult = _testObject.Create(new AlertPolicy(), CancellationToken.None).Result;
+
+            Assert.AreSame(expectedResult, actualResult);
+        }
+    }
+}
--- CenturyLinkCloudSdk/Services/AlertPolicyService.cs
+++ CenturyLinkCloudSdk/Services/AlertPolicyService.cs
@@ -1,11 +1,34 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CenturyLinkCloudSdk.Models;
+using CenturyLinkCloudSdk.Runtime;
+using CenturyLinkCloudSdk.Runtime.Client;
 
 namespace CenturyLinkCloudSdk.Services
 {
     public interface ICenturyLinkCloudAlertPolicyService
     {
          Task<AlertPolicy> Create(AlertPolicyDefniition definition, CancellationToken cancellationToken = default(CancellationToken));
+    }
+
+    public class AlertPolicyService : ICenturyLinkCloudAlertPolicyService
+    {
+        private readonly IHttpClient _httpClient;
+        private readonly IAliasProvider _aliasProvider;
+
+        public AlertPolicyService(IHttpClient httpClient, IAliasProvider aliasProvider)
+        {
+            _httpClient = httpClient;
+            _aliasProvider = aliasProvider;
+        }
+
+        public async Task<AlertPolicy> Create(AlertPolicyDefniition definition, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var alias = await _aliasProvider.GetAccountAlias();
+            var requestUri = String.Format("alertpolicies/{0}", alias);
+
+            return await _httpClient.PostAsync<AlertPolicy>(requestUri, definition, cancellationToken);
+        }
     }
 }
